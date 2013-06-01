56bf9bb3745e4df7295eab19d4adf7c1c921bfc0
Bilge Taylan Ulusoy <bilge.ulusoy@me.com>
2013-06-01 13:51:35 +0200

    Initial Commit

---
+++ TorKitTests/TorKitTests.h
@@ -0,0 +1,13 @@
+//
+//  TorKitTests.h
+//  TorKitTests
+//
+//  Created by Bilge Taylan Ulusoy on 01.06.13.
+//  Copyright (c) 2013 Bilge Taylan Ulusoy. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TorKitTests : SenTestCase
+
+@end
---
+++ TorKitTests/TorKitTests.m
@@ -0,0 +1,32 @@
+//
+//  TorKitTests.m
+//  TorKitTests
+//
+//  Created by Bilge Taylan Ulusoy on 01.06.13.
+//  Copyright (c) 2013 Bilge Taylan Ulusoy. All rights reserved.
+//
+
+#import "TorKitTests.h"
+
+@implementation TorKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TorKitTests");
+}
+
+@end
